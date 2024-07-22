6821ac7094fa6f7844825555402a18788ad65bae
SamuelJJk <samueljean.j24@gmail.com>
2024-07-22 18:46:03 -0400

    add timer, current doesn't do anything except countdown

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 function Timer() {
+  const [seconds, setSeconds] = useState(60);
+
+  useEffect(() => {
+    if (seconds > 0) {
+      const interval = setInterval(() => {
+        setSeconds((prevSeconds) => prevSeconds - 1);
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  }, [seconds]);
+
   return (
     <div className='timer'>
-      00
+      {seconds}
     </div>
-  )
+  );
 }
 
-export default Timer
+export default Timer;
