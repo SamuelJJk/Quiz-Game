fda6d6ce3307d8ab94332679724a750c41aca23e
SamuelJJk <samueljean.j24@gmail.com>
2024-07-22 12:29:57 -0400

    created state that hold the questions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import GameBoard from './components/GameBoard';
 import { simQ } from './questions/question';
 
 function App() {
-  
+  const [gameQuestions,setGameQuestions] = useState({})
+  //state above will hold the sample question to be passed down
+  useEffect(()=>{
+    setGameQuestions(simQ)
+  },[])  
   return (
     <div className="App">
       <GameBoard/>
